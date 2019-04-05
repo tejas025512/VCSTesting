2bd8f7c6e49f44d5a3f5ae019cb23a7330c44540
tejas025512 <44803556+tejas025512@users.noreply.github.com>
2019-04-05 19:04:10 +0530

    Add files via upload

---
+++ src/main/java/com/companyname/project_group2/VCSProjectTesting/App.java
@@ -0,0 +1,13 @@
+package com.companyname.project_group2.VCSProjectTesting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
